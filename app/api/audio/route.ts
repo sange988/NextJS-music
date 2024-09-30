56843518243ea1a2219bb73ae057ee805d50080f
clackypaas <clackypaas>
2024-09-30 12:07:00 +0800

    feat: enhance audio API and view implementation

--- app/api/audio/route.ts
+++ app/api/audio/route.ts
@@ -40,9 +40,11 @@ export async function POST(request: NextRequest) {
     console.log(`open ${path2} to see the uploaded file`)
 
     // 存入sqlite
-    let baseUrl = 'http://localhost:8080'
-    let src = `${baseUrl}/audio/${file.name}`
-    let coverImgUrl = `${baseUrl}/cover/${cover.name}`
+    let baseUrl = 'https://82db5aaf7a7038e604799a3e41f5e444-app.staging.clackypaas.com'
+    // let src = `${baseUrl}/audio/${file.name}`
+    // let coverImgUrl = `${baseUrl}/cover/${cover.name}`
+    let src = `${file.name}`
+    let coverImgUrl = `${cover.name}`
     // console.log(src,coverImgUrl)
 
     let music = await saveMusic(
